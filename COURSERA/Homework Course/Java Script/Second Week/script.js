a372aa92f9285fd3fdde313347d1922639ee9c29
Mustafa Özdemir <66470869+1DeliDolu@users.noreply.github.com>
2022-08-04 17:52:46 +0200

    Coursera Homework

---
+++ COURSERA/Homework Course/Java Script/Second Week/script.js
@@ -0,0 +1,28 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+    var img = previewPic.src
+    document.getElementById('image').style.background = "url('" + img + "')"
+    document.getElementById('image').innerHTML = previewPic.alt;
+   /* In this function you should 
+      1) change the url for the background image of the div with the id = "image" 
+      to the source file of the preview image
+      
+      2) Change the text  of the div with the id = "image" 
+      to the alt text of the preview image 
+      */
+    
+      }
+  
+      function unDo(){
+    document.getElementById('image').style.background ="#8e68ff"
+      document.getElementById('image').innerHTML = "Hover over an image below to display here.";
+       /* In this function you should 
+      1) Update the url for the background image of the div with the id = "image" 
+      back to the orginal-image.  You can use the css code to see what that original URL was
+      
+      2) Change the text  of the div with the id = "image" 
+      back to the original text.  You can use the html code to see what that original text was
+      */
+          
+      }
